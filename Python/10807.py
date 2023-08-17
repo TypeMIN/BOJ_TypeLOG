5564654099997998c6c874a79e845ee989fe134d
Minsik Lee <lms0522@unist.ac.kr>
2023-08-17 13:46:09 +0900

    23/08/17

---
+++ Python/10807.py
@@ -0,0 +1,21 @@
+N = int(input())
+
+input_str = input()
+input_list = input_str.split()
+
+v = int(input)
+
+answer = 0
+
+for i in range(N):
+  if int(input_list[i]) == v:
+    answer += 1
+
+print(answer)
+
+
+  
+  
+
+  
+  
