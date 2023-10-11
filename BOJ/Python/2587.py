66bab721b52cc067099bb7fb733d8a82dce58fa4
Minsik Lee <lms0522@unist.ac.kr>
2023-10-11 23:02:52 +0900

    Update 2587.py

--- BOJ/Python/2587.py
+++ BOJ/Python/2587.py
@@ -1,11 +1,16 @@
+# nums = 5개의 정수
 nums = []
 
+# 5개의 정수를 입력받아 nums에 저장
 for _ in range(5):
     nums.append(int(input()))
 
+# nums에 저장된 5개의 정수를 오름차순으로 정렬
 nums.sort()
 
+# avg = 5개 정수의 평균 = 5개 정수의 합 / 5
 avg = sum(nums) // 5
+# mid = 5개 정수의 중앙값 = 3번째로 작은 정수
 mid = nums[2]
 
 print(avg)
